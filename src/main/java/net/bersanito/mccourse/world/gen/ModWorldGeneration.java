4fcc623d4f45c2cd65929fadb2350aabe6e1c5f8
Bersanito <loewickes@gmail.com>
2024-02-16 22:57:38 +0100

    92.

--- src/main/java/net/bersanito/mccourse/world/gen/ModWorldGeneration.java
+++ src/main/java/net/bersanito/mccourse/world/gen/ModWorldGeneration.java
@@ -2,6 +2,9 @@ package net.bersanito.mccourse.world.gen;
 
 public class ModWorldGeneration {
     public static void generateModWorldGeneration() {
+        ModOreGeneration.generateOres();
+
         ModTreeGeneration.generateTrees();
+
     }
 }
